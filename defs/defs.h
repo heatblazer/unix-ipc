44c8f25f4a5b6cc0c331d820cfb74193ff15bba6
heatblazer <ilianzapryanov@abv.bg>
2016-06-14 10:06:07 -0400

    new project

--- defs/defs.h
+++ defs/defs.h
@@ -1,15 +1,5 @@
-#pragma once
+#ifndef DEFS
+#define DEFS
 
-
-#define RPIPE_NAME "/tmp/rpipe"
-#define WPIPE_NAME "/tmp/wpipe"
-
-#pragma pack(1)
-struct msg
-{
-    char message[256];
-    int  result;
-    int m_pid;
-    char univ[1];
-};
+#endif // DEFS
 
